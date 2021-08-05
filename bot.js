6f44d4f544829e7b7f8306240efb548b493da958
João Victor <juvito9813@gmail.com>
2021-08-04 21:29:25 -0300

    revert: command not found deleted

--- bot.js
+++ bot.js
@@ -2,14 +2,12 @@
 const Discord = require("discord.js"); //baixar a lib
 const client = new Discord.Client(); //conectando ao cliente
 const config = require("./config.json"); //fazendo o login do requiremento do token
-
-
-
+const { MessageEmbed } = require('discord.js');
 
 
 client.on('ready', () => {
-    client.user.setActivity("!info" , { type: "LISTENING"}) 
-  })
+    client.user.setActivity("!info", { type: "LISTENING" })
+})
 
 
 client.on("message", async message => {  //monitora tudo que está acontecendo no chat
@@ -21,11 +19,16 @@ client.on("message", async message => {  //monitora tudo que está acontecendo n
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const comando = args.shift().toLowerCase();
 
-    if (comando === "teste") {
-        message.channel.send("teste1234");
-    }else{
-        message.channel.send("Comando não encontrado - digite !info para saber mais");
+    if (comando === "info") {
+
+        const embed = new MessageEmbed();
+        embed.setColor('#0099ff');
+        embed.setTitle('Some title');
+        message.channel.send(embed)
     }
+
+     
+
 
 });
 
