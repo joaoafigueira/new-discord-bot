178b0ad99c20da5c1458d858ef64a605c71991b8
João Victor <juvito9813@gmail.com>
2021-08-18 13:30:02 -0300

    feat: add discord bot speak

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js"); //baixar a lib
 const client = new Discord.Client(); //conectando ao cliente
 const config = require("./config.json"); //fazendo o login do requiremento do token
 const { MessageEmbed } = require('discord.js');
+const path = require('path');
 
 
 client.on('ready', () => {
@@ -21,27 +22,63 @@ client.on("message", async message => {  //monitora tudo que está acontecendo n
     
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const comando = args.shift().toLowerCase();
-    const listaDeComandos = new Array(`info`);
-    let possuiComando = false;
 
 
 
 
-    if ( comando === listaDeComandos[0]) {
+    if ( comando === 'info') {
 
         const embed = new MessageEmbed();
-        possuiComando = true;
-        embed.setColor('#0099ff');
-        embed.setTitle('Some title');
+        embed.setColor('#000000');
+        embed.setTitle('Info');
+        embed.setAuthor('New', 'https://cdn.discordapp.com/attachments/872157580418486322/877577467693518878/discord-logo.png')
+        embed.setDescription('New is a discord bot for friends to friends.')
+        embed.setThumbnail('https://cdn.discordapp.com/attachments/872157580418486322/877577467693518878/discord-logo.png')
+        embed.addFields(
+            { name: 'Pessoas', value: '!gian', inline: true },
+            { name: 'Frases', value: '!construção', inline: true },
+        )
+        embed.setImage('https://cdn.discordapp.com/attachments/872157580418486322/877577467693518878/discord-logo.png')
         message.channel.send(embed)
 
     }
+
+    if(comando === 'gian'){
+      
+      const embed = new MessageEmbed();
+      embed.setColor('#FF6EB4');
+      embed.setTitle('Vocês não me atingem gente!!!');
+      embed.setDescription('o poha')
+      embed.setThumbnail('https://cdn.discordapp.com/attachments/720410505742778388/875543822845771776/unknown.png')
+      embed.addFields(
+          { name: 'Comandos', value: '!porra' }
+      )
+      message.channel.send(embed)
+     
+
+    }
        
+    if(comando === 'porra'){
+
+       const {voice} = message.member
+       
+       if(!voice.channelID){
+           message.reply('Você deve entrar em um canal de voz primeiro!');
+           return
+       }
+
+        voice.channel.join().then((connection) => {
+
+           connection.play(path.join(__dirname, 'oporra.m4a'))
+          
+        })
+
+        
+    }
      
 
 
 });
-
 
 client.login(config.token); //setando o token e terminando o bot
 
